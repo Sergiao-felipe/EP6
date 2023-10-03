ce74ba4dcccf6c1b0dc71f2eca89bc1091a6a812
Kaua toledo <127935235+Kaua123456789987@users.noreply.github.com>
2023-10-03 11:12:59 -0300

    te amo gu

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function funcaocorrigir(){
     var respostacerta = "correta";
+    var respostausuario = document.querySelector('input[name= "resposta"]:checked');
+
+    if(respostausuario){
+        if(respostauiauario.value === respostacerta){
+            document.getElementById("retorno").inertHTML = "acertou, mizeravikkk";
+        }
+        else{
+            document.getElementById("retorno").inertHTML = "errou, burraokkk"
+        }
+    }
+    else{
+        alert("tem que escolher uma opção, cabaço!");
+    }
 }

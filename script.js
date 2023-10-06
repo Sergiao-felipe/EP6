ffbda833f8d4e600b262f1cba11f5dcb70db5f43
Gustavobarbeiro <128605634+Sergiao-felipe@users.noreply.github.com>
2023-10-06 12:07:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function funcaoCorrigir(){
-var respostaCerta = "correta";
-var respostaUsuario = document.querySelector(*1nput[name=*resposta"]:checked');
-If (respostaUsuario){
-If (respostaUsuario, value === respostaCerta)[
-document. getElementById("retorno").InnerHTML = " Acertou, mizeravi!";
-8
-elsel document. getElementById(*retorno") InnerHTML = " Errou, Cabaço!";
-    
-elset alert(*tem que escolher uma opação, cabaçol *):
+    var respostaCerta = "correta";
+    var respostaUsuario = document.querySelector('input[name=*resposta"]:checked');
+    if (respostaUsuario){
+    if (respostaUsuario. value === respostaCerta) {
+    document. getElementById("retorno").InnerHTML = " Acertou, mizeravi!";
+    }
+    else{ document. getElementById("retorno") InnerHTML = " Errou, Cabaço!";
+    }    
+    elset alert(*tem que escolher uma opação, cabaçol *):
+    }
+}
